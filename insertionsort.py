559a3530f9ebccf03d1f3bc7533ed52a15c0406e
nalinasai <nalinanalina995@gmail.com>
2025-02-22 11:38:21 +0530

    add the comments for pyhton codes

--- insertionsort.py
+++ insertionsort.py
@@ -1,19 +1,19 @@
-#first line input contains size of the input
-#second line input contains To get the input of a list from the user with spaces.
-
+# First line input: The user is asked to input the size of the list (number of elements)
+# Second line input: The user is asked to input a space-separated string of integers
 
 size_of_list = int(input("input the size of list: "))
 unsorted_string_list = input("input the unsorted integers with spaces: ")
 
-
+# Initialize an empty list to store the integers
 input_list = []
 unsorted_list = unsorted_string_list.split()
 
+# Convert the string elements into integers and append them to input_list
 for element in unsorted_list:
     input_list.append(int(element))
 
 
-
+# Loop through the list starting from the second element (index 1)
 for i in range(1,size_of_list):
     key = input_list[i]
     j = i-1
