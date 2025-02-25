1d981d4e6261a80ff18207ff5b48f39b29987420
nalinasai <nalinanalina995@gmail.com>
2025-02-25 12:58:07 +0530

    modify the code that get the input from the user

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,21 +1,28 @@
 //Insertion sort in C++
 
 #include <iostream>   //include the input-output stream for displaying the output
+#include <vector>
+#include <sstream>
 using namespace std;
 
 int main(){
+    cout << "Enter the input: ";
+    vector<int> array;
+    string inputs;
 
-    int array[5] = {2,4,3,1,7}; //initialize the array with 5 elements
+    getline(cin, inputs);
+    stringstream stream(inputs);
 
-    cout << "Before the sorted array"<< endl;
-    for(int p=0; p<5; p++){
-        cout << array[p] << " ";
+    int num;
+    while (stream>>num){
+        array.push_back(num);
     }
-    cout<<endl;
-    
-    cout << "After sorted array"<<endl;
+
+    int n = array.size();
+
+    cout << "After sorted array: ";
     // Outer loop for iterating through each element of the array starting from index 1
-    for (int i=1; i<5; i++){
+    for (int i=1; i<n; i++){
         int key = array[i];    // Set the current element as the 'key'
         int j =i-1;            //// Set the index for comparison (one position before 'i')
         while(j>=0 && key<array[j]){
@@ -26,7 +33,7 @@ int main(){
     }
 
     // // Output the sorted array
-    for(int k=0; k<5; k++){
+    for(int k=0; k<n; k++){
         cout << array[k] << " ";
     }
 
